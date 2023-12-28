d2a474bbe5ad601f9d2ca8854efdb8b37d33851f
Dominik Schubert <dominik.schubert91@gmail.com>
2023-12-28 10:44:45 +0100

    bump version to v0.22.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     setup(
         name='awscli-local',
-        version='0.21.1',
+        version='0.22.0',
         description=description,
         long_description=README,
         long_description_content_type='text/markdown',
